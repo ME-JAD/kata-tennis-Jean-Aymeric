c2ec21a1893d666fb0650e7f74607e082017347a
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2025-06-13 14:13:54 +0200

    Kata 1 done

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,10 +1,16 @@
-
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+
+    static final int ADVANTAGE_POINTS_LIMIT = 3;
+    static final String SCORE_SEPARATOR = "-";
+    static final String DEUCE_SCORE = "Deuce";
+    static final String LOVE = "Love";
+    static final String FIFTEEN = "Fifteen";
+    static final String THIRTY = "Thirty";
+    static final String FORTY = "Forty";
+    private final String player1Name;
+    private final String player2Name;
+    private int nbPointsPlayer1 = 0;
+    private int nbPointsPlayer2 = 0;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,65 +18,48 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (this.player1Name.equals(playerName)) this.nbPointsPlayer1 += 1;
+        if (this.player2Name.equals(playerName)) this.nbPointsPlayer2 += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (this.nbPointsPlayer1 == this.nbPointsPlayer2) return this.generateEqualScore();
+
+        if (this.nbPointsPlayer1 > TennisGame1.ADVANTAGE_POINTS_LIMIT
+                || this.nbPointsPlayer2 > TennisGame1.ADVANTAGE_POINTS_LIMIT) {
+            return this.generateMoreThanPointsAdvantageLimitScore();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
+
+        return this.generateLessThanPointsAdvantageLimitScore();
+    }
+
+    private String generateEqualScore() {
+        if (this.nbPointsPlayer1 >= TennisGame1.ADVANTAGE_POINTS_LIMIT) return TennisGame1.DEUCE_SCORE;
+        return TennisGame1.getScoreFromPoints((this.nbPointsPlayer1)) + TennisGame1.SCORE_SEPARATOR + "All";
+    }
+
+    private String generateMoreThanPointsAdvantageLimitScore() {
+        String score;
+        int minusResult = this.nbPointsPlayer1 - this.nbPointsPlayer2;
+        score = (Math.abs(minusResult) == 1) ? "Advantage" : "Win for";
+        score += " ";
+        score += (minusResult > 0) ? "player1" : "player2";
         return score;
+    }
+
+    private String generateLessThanPointsAdvantageLimitScore() {
+        return TennisGame1.getScoreFromPoints(
+                this.nbPointsPlayer1) + TennisGame1.SCORE_SEPARATOR + TennisGame1.getScoreFromPoints(
+                this.nbPointsPlayer2);
+    }
+
+    private static String getScoreFromPoints(final int points) {
+        return switch (points) {
+            case 0 -> TennisGame1.LOVE;
+            case 1 -> TennisGame1.FIFTEEN;
+            case 2 -> TennisGame1.THIRTY;
+            case 3 -> TennisGame1.FORTY;
+            default -> TennisGame1.DEUCE_SCORE;
+        };
     }
 }
